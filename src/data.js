c66ef263366f847e0098699c865840a06bf7a93e
Harry <78834470+Harry9121@users.noreply.github.com>
2025-09-07 11:36:31 -0700

    New data.js for reusing data, move unused index2.html and index3.html to old.

---
+++ src/data.js
@@ -0,0 +1,41 @@
+// Sharing data for reuse
+export const companions = [
+    { name:"Momo", value:100, goldValue:3000, atomicValue:4000, image:"images/momo.png", isAura:false },
+    { name:"Eddie", value:100, goldValue:3000, atomicValue:4000, image:"images/eddie.png", isAura:false },
+    { name:"Jojo", value:100, goldValue:3000, atomicValue:4000, image:"images/jojo.png", isAura:false },
+    { name:"Mino", value:100, goldValue:3000, atomicValue:4000, image:"images/mino.png", isAura:false },
+    { name:"Ana", value:100, goldValue:3000, atomicValue:4000, image:"images/ana.png", isAura:false },
+    { name:"Turbo", value:2500, goldValue:15000, atomicValue:25000, image:"images/turbo.png", isAura:false },
+    { name:"Seidon", value:3500, goldValue:20000, atomicValue:22000, image:"images/seidon.png", isAura:false },
+    { name:"Ultra Gogo", value:5000, goldValue:20000, atomicValue:30000, image:"images/ui.png", isAura:false },
+    { name:"Mahoraga", value:5000, goldValue:20000, atomicValue:30000, image:"images/m.png", isAura:false },
+    { name:"Freeah", value:10000, goldValue:50000, atomicValue:60000, image:"images/freeah.png", isAura:false },
+    { name:"Sid", value:30000, goldValue:70000, atomicValue:90000, image:"images/cid.png", isAura:false },
+    { name:"Emperor Teach", value:200000, goldValue:270000, atomicValue:290000, image:"images/emp.png", isAura:false },
+    { name:"Super Baybe", value:220000, goldValue:280000, atomicValue:300000, image:"images/sbb.png", isAura:false },
+    { name:"Naru Baryon", value:330000, goldValue:400000, atomicValue:420000, image:"images/naru.png", isAura:false },
+    { name:"BroBro", value:520000, goldValue:640000, atomicValue:650000, image:"images/brobro.png", isAura:false },
+    { name:"Demon Galo", value:520000, goldValue:640000, atomicValue:650000, image:"images/galo.png", isAura:false },
+    { name:"Better World", value:640000, goldValue:770000, atomicValue:780000, image:"images/betterworld.png", isAura:false },
+
+    // Auras as companions too (for trading grid)
+    { name:"Atomic", value:100, image:"images/atomic.png", isAura:true },
+    { name:"Death", value:5000, image:"images/death.png", isAura:true },
+    { name:"Solo", value:10000, image:"images/solo.png", isAura:true },
+    { name:"Babylon", value:50000, image:"images/baby.png", isAura:true },
+    { name:"Infinity", value:420000, image:"images/inf.png", isAura:true },
+    { name:"Sacred", value:180000, image:"images/sacred.png", isAura:true },
+    { name:"Chronology", value:520000, image:"images/chrono.png", isAura:true },
+    { name:"Prodigy", value:640000, image:"images/prodigy.png", isAura:true },
+];
+
+export const auras = [
+    { name:"Atomic", value:100, image:"images/atomic.png" },
+    { name:"Death", value:5000, image:"images/death.png" },
+    { name:"Solo", value:10000, image:"images/solo.png" },
+    { name:"Babylon", value:50000, image:"images/baby.png" },
+    { name:"Infinity", value:420000, image:"images/inf.png" },
+    { name:"Sacred", value:180000, image:"images/sacred.png" },
+    { name:"Chronology", value:520000, image:"images/chrono.png" },
+    { name:"Prodigy", value:640000, image:"images/prodigy.png" },
+];
